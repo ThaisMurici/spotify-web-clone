c7cc4afe1cab6602f9812f9e84db0c37e66c3187
Thais Moraes <murici.thais@gmail.com>
2019-05-07 16:16:42 -0300

    Set up Redux and Redux Saga

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './config/reactotron';
@@ -9,23 +10,27 @@ import { Wrapper, Container, Content } from './styles/components';
 
 import Header from './components/Header';
 import Player from './components/Player';
-import Routes from './routes';
 import SideBar from './components/Sidebar';
 
+import Routes from './routes';
+import store from './store';
+
 const App = () => (
-  <BrowserRouter>
-    <Wrapper>
-      <GlobalStyle />
-      <Container>
-        <SideBar />
-        <Content>
-          <Header />
-          <Routes />
-        </Content>
-      </Container>
-      <Player />
-    </Wrapper>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Wrapper>
+        <GlobalStyle />
+        <Container>
+          <SideBar />
+          <Content>
+            <Header />
+            <Routes />
+          </Content>
+        </Container>
+        <Player />
+      </Wrapper>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './ducks';
+import rootSaga from './sagas';
+
+const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(sagaMiddleware),
+    console.tron.createEnhancer(),
+  ),
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
