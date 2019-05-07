2353319f750835f71b3c0b2aae949022535cfe59
Thais Moraes <murici.thais@gmail.com>
2019-05-07 14:30:06 -0300

    Add styling to Browse page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import { GlobalStyle } from './styles/global';
 
@@ -6,19 +7,23 @@ import { Wrapper, Container, Content } from './styles/components';
 
 import Header from './components/Header';
 import Player from './components/Player';
+import Routes from './routes';
 import SideBar from './components/Sidebar';
 
 const App = () => (
-  <Wrapper>
-    <GlobalStyle />
-    <Container>
-      <SideBar />
-      <Content>
-        <Header />
-      </Content>
-    </Container>
-    <Player />
-  </Wrapper>
+  <BrowserRouter>
+    <Wrapper>
+      <GlobalStyle />
+      <Container>
+        <SideBar />
+        <Content>
+          <Header />
+          <Routes />
+        </Content>
+      </Container>
+      <Player />
+    </Wrapper>
+  </BrowserRouter>
 );
 
 export default App;
