ab7bc541417a3b97e3ecd708eb717214228c90b1
Thais Moraes <murici.thais@gmail.com>
2019-05-08 17:36:07 -0300

    Add option to play/pause a song

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -4,6 +4,8 @@ import Slider from 'rc-slider';
 import Sound from 'react-sound';
 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as PlayerActions } from '../../store/ducks/player';
 
 import {
   Container, Current, Volume, Progress, Controls, Time, ProgressSlider,
@@ -17,7 +19,7 @@ import PauseIcon from '../../assets/images/pause.svg';
 import ForwardIcon from '../../assets/images/forward.svg';
 import RepeatIcon from '../../assets/images/repeat.svg';
 
-const Player = ({ player }) => (
+const Player = ({ player, play, pause }) => (
   <Container>
     {!!player.currentSong && <Sound url={player.currentSong.file} playStatus={player.status} />}
 
@@ -42,14 +44,20 @@ const Player = ({ player }) => (
         <button>
           <img src={BackwardIcon} alt="Shuffle" />
         </button>
+        {!!player.currentSong && player.status === Sound.status.PLAYING ? (
+          <button onClick={pause}>
+            <img src={PauseIcon} alt="Pause" />
+          </button>
+        ) : (
+          <button onClick={play}>
+            <img src={PlayIcon} alt="Play" />
+          </button>
+        )}
         <button>
-          <img src={PlayIcon} alt="Shuffle" />
+          <img src={ForwardIcon} alt="Forward" />
         </button>
         <button>
-          <img src={ForwardIcon} alt="Shuffle" />
-        </button>
-        <button>
-          <img src={RepeatIcon} alt="Shuffle" />
+          <img src={RepeatIcon} alt="Repeat" />
         </button>
       </Controls>
 
@@ -88,10 +96,17 @@ Player.propTypes = {
     }),
     status: PropTypes.string,
   }).isRequired,
+  play: PropTypes.func.isRequired,
+  pause: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   player: state.player,
 });
 
-export default connect(mapStateToProps)(Player);
+const mapDispatchToProps = dispatch => bindActionCreators(PlayerActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Player);
--- src/store/ducks/player.js
+++ src/store/ducks/player.js
@@ -2,6 +2,8 @@ import Sound from 'react-sound';
 
 export const Types = {
   LOAD: 'player/LOAD',
+  PLAY: 'player/PLAY',
+  PAUSE: 'player/PAUSE',
 };
 
 export const Creators = {
@@ -9,6 +11,10 @@ export const Creators = {
     type: Types.LOAD,
     payload: { song },
   }),
+
+  play: () => ({ type: Types.PLAY }),
+
+  pause: () => ({ type: Types.PAUSE }),
 };
 
 const INITIAL_STATE = {
@@ -20,6 +26,10 @@ export default function player(state = INITIAL_STATE, action) {
   switch (action.type) {
     case Types.LOAD:
       return { ...state, currentSong: action.payload.song, status: Sound.status.PLAYING };
+    case Types.PLAY:
+      return { ...state, status: Sound.status.PLAYING };
+    case Types.PAUSE:
+      return { ...state, status: Sound.status.PAUSED };
     default:
       return state;
   }
