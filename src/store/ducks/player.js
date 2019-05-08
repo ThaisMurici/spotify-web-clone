25248883f5125652d4467f66e18566c21eeddccb
Thais Moraes <murici.thais@gmail.com>
2019-05-08 20:28:58 -0300

    Add player slider controls

--- src/store/ducks/player.js
+++ src/store/ducks/player.js
@@ -7,6 +7,8 @@ export const Types = {
   NEXT: 'player/NEXT',
   PREV: 'player/PREV',
   PLAYING: 'player/PLAYING',
+  HANDLE_POSITION: 'player/HANDLE_POSITION',
+  SET_POSITION: 'player/SET_POSITION',
 };
 
 export const Creators = {
@@ -27,6 +29,16 @@ export const Creators = {
     type: Types.PLAYING,
     payload: { position, duration },
   }),
+
+  handlePosition: percent => ({
+    type: Types.HANDLE_POSITION,
+    payload: { percent },
+  }),
+
+  setPosition: percent => ({
+    type: Types.SET_POSITION,
+    payload: { percent },
+  }),
 };
 
 const INITIAL_STATE = {
@@ -34,6 +46,7 @@ const INITIAL_STATE = {
   list: [],
   status: Sound.status.PLAYING,
   position: null,
+  positionShown: null,
   duration: null,
 };
 
@@ -56,7 +69,12 @@ export default function player(state = INITIAL_STATE, action) {
       const prev = state.list[currentIndex - 1];
 
       if (prev) {
-        return { ...state, currentSong: prev, status: Sound.status.PLAYING };
+        return {
+          ...state,
+          currentSong: prev,
+          status: Sound.status.PLAYING,
+          position: 0,
+        };
       }
 
       return state;
@@ -67,13 +85,22 @@ export default function player(state = INITIAL_STATE, action) {
       const next = state.list[currentIndex + 1];
 
       if (next) {
-        return { ...state, currentSong: next, status: Sound.status.PLAYING };
+        return {
+          ...state,
+          currentSong: next,
+          status: Sound.status.PLAYING,
+          position: 0,
+        };
       }
 
       return state;
     }
     case Types.PLAYING:
       return { ...state, ...action.payload };
+    case Types.HANDLE_POSITION:
+      return { ...state, positionShown: state.duration * action.payload.percent };
+    case Types.SET_POSITION:
+      return { ...state, position: state.duration * action.payload.percent, positionShown: null };
     default:
       return state;
   }
